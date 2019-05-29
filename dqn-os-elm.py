9553dda45ace8d8a8b2a9c0b850f85538f3a26af
Stern Gerlach <std.experimental.optional@gmail.com>
2019-05-29 14:56:36 +0900

    use double dqn, prioritized experience replay

--- dqn-os-elm.py
+++ dqn-os-elm.py
@@ -27,6 +27,46 @@ class ExperienceReplayMemory(object):
     def __len__(self):
         return len(self.experiences)
     
+class TDErrorMemory(object):
+    def __init__(self, capacity, td_error_bias=1e-03):
+        self.capacity = capacity
+        self.td_errors = deque(maxlen=capacity)
+        self.td_error_bias = td_error_bias
+        
+    def append(self, td_error):
+        self.td_errors.append(td_error)
+        
+    def get_indices(self, batch_size):
+        sum_absolute_td_errors = np.sum(np.abs(self.td_errors))
+        sum_absolute_td_errors += self.td_error_bias * len(self.td_errors)
+        
+        rand_values = np.random.uniform(
+            low=0.0, high=sum_absolute_td_errors, size=batch_size)
+        rand_values = np.sort(rand_values)
+        
+        indices = []
+        i = 0
+        tmp_sum = 0.0
+        
+        for r in rand_values:
+            while tmp_sum < r:
+                tmp_sum += (abs(self.td_errors[i]) + self.td_error_bias)
+                i += 1
+            
+            if i >= len(self.td_errors):
+                i = len(self.td_errors) - 1
+            
+            indices.append(i)
+        
+        return indices
+    
+    def update_td_errors(self, updated_td_errors):
+        for i, td_error in enumerate(updated_td_errors):
+            self.td_errors[i] = td_error
+        
+    def __len__(self):
+        return len(self.td_errors)
+    
 class DqnOsElmAgent(object):
     def __init__(self, num_states, num_actions, num_hidden,
                  batch_size=1, initial_epsilon=0.5, gamma=0.9,
@@ -40,11 +80,19 @@ class DqnOsElmAgent(object):
         
         self.experience_replay = ExperienceReplayMemory(
             capacity=experience_replay_capacity)
+        self.td_error_memory = TDErrorMemory(
+            capacity=experience_replay_capacity)
         
-        self.alpha = np.random.uniform(
-            low=-1.0, high=1.0, size=(num_states, num_hidden)) * 0.05
-        self.b = np.random.uniform(
-            low=-1.0, high=1.0, size=num_hidden) * 0.05
+        # self.alpha = np.random.uniform(
+        #     low=-1.0, high=1.0, size=(num_states, num_hidden)) * 0.05
+        # self.b = np.random.uniform(
+        #     low=-1.0, high=1.0, size=num_hidden) * 0.05
+        
+        self.alpha = np.random.normal(
+            loc=0.0, scale=0.05, size=(num_states, num_hidden))
+        self.b = np.random.normal(
+            loc=0.0, scale=0.05, size=num_hidden)
+        
         self.P = np.zeros((num_hidden, num_hidden))
         self.beta = np.zeros((num_hidden, num_actions))
         
@@ -63,7 +111,7 @@ class DqnOsElmAgent(object):
         # for i, e in enumerate(experiences):
         #     G = 0.0
         #     t = 0
-            
+        
         #     for j in range(i, len(experiences)):
         #         G += np.power(self.gamma, t) * experiences[j].reward
         #         t += 1
@@ -73,10 +121,15 @@ class DqnOsElmAgent(object):
         # self.P = np.linalg.inv(hidden_outputs.T @ hidden_outputs)
         # self.beta = self.P @ hidden_outputs.T @ expected_state_values
         
-        self.P = np.random.uniform(
-            low=-1.0, high=1.0, size=(self.num_hidden, self.num_hidden)) * 0.05
-        self.beta = np.random.uniform(
-            low=-1.0, high=1.0, size=(self.num_hidden, self.num_actions)) * 0.05
+        # self.P = np.random.uniform(
+        #     low=-1.0, high=1.0, size=(self.num_hidden, self.num_hidden)) * 0.05
+        # self.beta = np.random.uniform(
+        #     low=-1.0, high=1.0, size=(self.num_hidden, self.num_actions)) * 0.05
+        
+        self.P = np.random.normal(
+            loc=0.0, scale=0.05, size=(self.num_hidden, self.num_hidden))
+        self.beta = np.random.normal(
+            loc=0.0, scale=0.05, size=(self.num_hidden, self.num_actions))
         
         self.update_target()
         
@@ -100,11 +153,13 @@ class DqnOsElmAgent(object):
         hidden_layer_output = activation_func(state_batch.dot(alpha) + b)
         return hidden_layer_output
         
-    def update(self):
+    def update(self, episode):
         if len(self.experience_replay) < self.batch_size:
             return
         
-        experiences = self.experience_replay.get_batch(self.batch_size)
+        # experiences = self.experience_replay.get_batch(self.batch_size)
+        indices = self.td_error_memory.get_indices(self.batch_size)
+        experiences = [self.experience_replay.experiences[i] for i in indices]
         
         states = np.array([e.state for e in experiences])
         rewards = np.array([e.reward for e in experiences])
@@ -118,18 +173,55 @@ class DqnOsElmAgent(object):
         hidden_outputs = self.hidden_output(
             states, self.alpha, self.b, self.sigmoid)
         
+        max_actions = np.zeros(len(non_final_next_states))
+        max_actions = self.predict(
+            non_final_next_states, self.alpha, self.b,
+            self.beta, self.sigmoid).argmax(axis=1)
+        
         next_state_values = np.zeros((self.batch_size, self.num_actions))
         next_state_values[done_flags] = self.predict(
             non_final_next_states, self.alpha_old, self.b_old,
-            self.beta_old, self.sigmoid).max(axis=1)
+            self.beta_old, self.sigmoid)[np.arange(len(non_final_next_states)), max_actions]
         
         expected_state_values = rewards + self.gamma * next_state_values
         
         self.P -= (self.P @ hidden_outputs.T @ hidden_outputs @ self.P) \
             / (1.0 + hidden_outputs @ self.P @ hidden_outputs.T)
-            
+        
         self.beta += self.P @ hidden_outputs.T \
             @ (expected_state_values - hidden_outputs @ self.beta)
+            
+    def update_td_error_memory(self):
+        experiences = self.experience_replay.experiences
+        
+        states = np.array([e.state for e in experiences])
+        actions = np.array([e.action for e in experiences])
+        rewards = np.array([e.reward for e in experiences])
+        done_flags = [not e.done for e in experiences]
+        non_final_next_states = np.array([e.next_state for e in experiences
+                                          if e.done == False])
+    
+        if len(non_final_next_states) == 0:
+            return
+        
+        state_values = self.predict(
+            states, self.alpha, self.b, self.beta, self.sigmoid) \
+            [np.arange(len(experiences)), actions]
+        
+        max_actions = np.zeros(len(non_final_next_states))
+        max_actions = self.predict(
+            non_final_next_states, self.alpha, self.b,
+            self.beta, self.sigmoid).argmax(axis=1)
+        
+        next_state_values = np.zeros(len(experiences))
+        next_state_values[done_flags] = self.predict(
+            non_final_next_states, self.alpha_old, self.b_old,
+            self.beta_old, self.sigmoid) \
+            [np.arange(len(non_final_next_states)), max_actions]
+        
+        td_errors = rewards + self.gamma * next_state_values - state_values
+        
+        self.td_error_memory.update_td_errors(td_errors)
             
     def get_action(self, state, episode):
         epsilon = self.initial_epsilon * (1.0 / (episode + 1.0))
@@ -149,6 +241,9 @@ class DqnOsElmAgent(object):
         
     def append_experience(self, state, action, next_state, reward, done):
         self.experience_replay.append(state, action, next_state, reward, done)
+        
+    def append_td_error(self, td_error):
+        self.td_error_memory.append(td_error)
         
     def play(self, env, episode_num=5):
         for i in range(episode_num):
@@ -213,19 +308,22 @@ class DqnOsElmTrainer(object):
                 action = self.agent.get_action(state, episode)
                 next_state, reward, done, info = self.env.step(action)
                 
-                # reward = 0.0 if not done else -1.0 if step < 195 else 1.0
+                reward = 0.0 if not done else -1.0 if step < 195 else 1.0
                 
                 self.agent.append_experience(state, action, next_state, reward, done)
-                
-                for i in range(10):
-                    self.agent.update()
+                self.agent.append_td_error(0.0)
+            
+                for i in range(3):
+                    self.agent.update(episode)
                 
                 if done:
                     recent_rewards = np.hstack((recent_rewards[1:], step + 1))
                     print("Episode {}, step: {}, average step: {}".format(
                         episode, step + 1, recent_rewards.mean()))
                     
-                    if episode % 5 == 0:
+                    self.agent.update_td_error_memory()
+                    
+                    if episode % 2 == 0:
                         self.agent.update_target()
                     
                     break
